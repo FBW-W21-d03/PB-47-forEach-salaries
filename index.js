a50ee21fc5c32b112b96c51e77d60d13507802c1
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-10 14:26:44 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,17 @@
+// AUFGABE TEIL 1
+const salary2021 = [1980.00, 2050.00, 3045.50, 1050.00, 4678.90, 2800.00, 2699.50];
+
+// AUFGABE TEIL 2
+let salary2022 = [];
+
+// AUFGABE TEIL 3
+let gehalt = 1.04
+salary2021.forEach(
+    (gehaltSalary2021) => {
+        salary2022.push(gehalt * gehaltSalary2021)
+        
+    }
+);
+//  AUFGABE TEIL 4
+ console.log("salary2021:" , `${salary2021}`);
+ console.log(" salary2022:", `${salary2022}`);
