867c1911f8932d57f536f10a698e440bd1f88d33
sprotte <metatron1972@live.de>
2022-01-10 14:03:38 +0100

    sprotte

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const salary2021 = [1980.0, 2050.0, 3045.5, 1050.0, 4678.9, 2800.0, 2699.5];
 
 const salary2022 = [];
 
-let gehalt = 1;
+let gehalt = 4;
 salary2021.forEach((gehalt) => {
   gehalt = gehalt + salary2021;
   console.log("gehalt", gehalt + salary2021);
