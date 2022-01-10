2d376351be90a422e3085bbf01b68864edbeab6c
slavablack <veaceslav.ciornii@magenta.de>
2022-01-10 14:31:19 +0100

    salary 2022

---
+++ index.js
@@ -0,0 +1,12 @@
+const salary2021 = [1980.00, 2050.00, 3045.50, 1050.00, 4678.90, 2800.00, 2699.50];
+
+const salary2022 = [];
+
+
+salary2021.forEach (
+    (oldsalary) => {
+            salary2022.push (4/100*oldsalary +oldsalary);
+    }
+);
+
+console.log(salary2022);
